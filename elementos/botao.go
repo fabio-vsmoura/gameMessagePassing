134c8e6d7192958980e46d60746d341fe5772883
fabio.vsmoura@gmail.com <design.criation@gmail.com>
2026-09-07 20:37:10 -0300

    Criação de botão e script

---
+++ elementos/botao.go
@@ -0,0 +1,15 @@
+components {
+  id: "botao"
+  component: "/scripts/botao.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"botao_usar\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/elementos/elementos.atlas\"\n"
+  "}\n"
+  ""
+}
