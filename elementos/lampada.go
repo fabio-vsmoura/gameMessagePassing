ea0861dc74ed084f431b9961434537870f823955
fabio.vsmoura@gmail.com <design.criation@gmail.com>
2026-09-07 20:40:58 -0300

    Criação de lâmpada e script

---
+++ elementos/lampada.go
@@ -0,0 +1,15 @@
+components {
+  id: "lampada"
+  component: "/scripts/lampada.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"cristal\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/elementos/elementos.atlas\"\n"
+  "}\n"
+  ""
+}
